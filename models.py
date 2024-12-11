68c6dd8dd2c661f7054cfc45050c6c1055c25a77
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-12-11 09:59:36 +0300

    initial commit

---
+++ models.py
@@ -0,0 +1,19 @@
+# define our tables using OOP + sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Text, Integer, VARCHAR
+
+# create a base model that all our models are going to inherit from
+Base = declarative_base()
+
+# define model
+"""
+1. Its a must we provide the table name via the attribute __tablename__
+2. It`s must we provide at least one table column
+"""
+class User(Base):
+    __tablename__ = "users"
+
+    # define columns
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(Text(), nullable=False) # NOT NULL
+    email = Column(VARCHAR(), nullable=False, unique=True) # NOT NULL, UNIQUE
